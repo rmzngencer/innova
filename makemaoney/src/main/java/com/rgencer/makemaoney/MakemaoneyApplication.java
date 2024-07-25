a049783c9eb9542f045219737030cac36433f47f
ramazan gencer <on.rgencer@biofarma.com.tr>
2024-07-25 10:44:05 +0300

    create project

---
+++ makemaoney/src/main/java/com/rgencer/makemaoney/MakemaoneyApplication.java
@@ -0,0 +1,13 @@
+package com.rgencer.makemaoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakemaoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakemaoneyApplication.class, args);
+	}
+
+}
